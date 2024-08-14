eeb0ca3e3946d6e75c033910f6aa619227ec123e
Aleh Mazok <mazokoleg@gmail.com>
2024-08-14 15:16:30 +0200

    refactor: Класс-вью замест вью функцый.

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 
-from .views import get_word_by_id, get_word_by_external_id
+from .views import WordByIdRetrieveAPIView, WordByExternalIdRetrieveAPIView
 
 urlpatterns = [
-    path('words/<int:word_id>/', get_word_by_id, name='get_word_by_id'),
-    path('words/<str:direction>/<int:external_id>/', get_word_by_external_id, name='get_word_by_external_id'),
+    path(
+        'words/<int:pk>/',
+        WordByIdRetrieveAPIView.as_view(),
+        name='word_by_id',
+    ),
+    path(
+        'words/<str:direction>/<int:external_id>/',
+        WordByExternalIdRetrieveAPIView.as_view(),
+        name='word_by_external_id',
+    ),
 ]
--- main/views.py
+++ main/views.py
@@ -1,26 +1,30 @@
-from django.http.response import Http404
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework.exceptions import NotFound
 
 from .models import Word
 from .serializers import WordSerializer
 
 
-@api_view(['GET'])
-def get_word_by_id(req, word_id):
-    try:
-        word = Word.objects.get(id=word_id)
-        serializer = WordSerializer(word)
-        return Response(serializer.data)
-    except Word.DoesNotExist as e:
-        raise Http404()
+class WordByIdRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = Word.objects.all()
+    serializer_class = WordSerializer
 
 
-@api_view(['GET'])
-def get_word_by_external_id(req, direction: str, external_id: int):
-    try:
-        word = Word.objects.get(direction=direction, external_id=external_id)
-        serializer = WordSerializer(word)
-        return Response(serializer.data)
-    except Word.DoesNotExist as e:
-        raise Http404()
+class WordByExternalIdRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = WordSerializer
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        direction = self.kwargs.get('direction')
+        external_id = self.kwargs.get('external_id')
+
+        # Filter the queryset by direction and external_id
+        obj = queryset.filter(direction=direction, external_id=external_id).first()
+
+        if obj is None:
+            raise NotFound("Word not found with the specified direction and external_id")
+
+        return obj
+
+    def get_queryset(self):
+        return Word.objects.all()
