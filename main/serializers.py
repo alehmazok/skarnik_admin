a295a5d071d187b62a23fb6a33bee7c0d06f34a6
Aleh Mazok <mazokoleg@gmail.com>
2025-01-23 13:27:19 +0100

    chore: Выводзіць абодва слова, з націскам і без у API

--- main/serializers.py
+++ main/serializers.py
@@ -4,14 +4,6 @@ from .models import Word
 
 
 class WordSerializer(serializers.ModelSerializer):
-    text = serializers.SerializerMethodField()
-
     class Meta:
         model = Word
         fields = '__all__'
-
-    @staticmethod
-    def get_text(obj: Word):
-        if obj.stress and obj.stress != obj.text:
-            return obj.stress
-        return obj.text
